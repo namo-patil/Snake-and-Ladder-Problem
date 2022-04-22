b05343e0224c5167bf854e5db46333ba5935fb33
namo-patil <namratapatil74738@gmail.com>
2022-04-22 22:55:24 +0530

    Added: Program to get dice number between 1 to 6

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -1,13 +1,22 @@
 package com.bridgelabz.snakeandladdergame;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
     public static void playerStartPosition() {
         int startPosition = 0;
-        System.out.println(startPosition);
+    }
+
+    public static int dieRoll() {
+        Random random = new Random();
+        int diceValue = random.nextInt(6) + 1;
+        System.out.println(diceValue);
+        return diceValue;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
         playerStartPosition();
+        dieRoll();
     }
 }
