24d8f1f5a6c752af59d2ba085e3674cf65824b32
namo-patil <namratapatil74738@gmail.com>
2022-04-23 00:10:50 +0530

    Added: Program to check for a options like player 'Not-played' or 'on Ladder' or 'on Snake'

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -3,20 +3,43 @@ package com.bridgelabz.snakeandladdergame;
 import java.util.Random;
 
 public class SnakeAndLadder {
-    public static void playerStartPosition() {
-        int startPosition = 0;
-    }
+    public static final int START_POSITION = 0;
+    public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
+    static String POSITION;
+    static Random random = new Random();
 
     public static int dieRoll() {
-        Random random = new Random();
         int diceValue = random.nextInt(6) + 1;
-        System.out.println(diceValue);
+        System.out.println("Dice value : " + diceValue);
         return diceValue;
+    }
+
+    public static int playerChecksOption() {
+        int currentPosition = START_POSITION;
+        int checkOption = random.nextInt(3);
+        int diceNumber = dieRoll();
+        switch (checkOption) {
+            case NO_PLAY:
+                currentPosition = currentPosition;
+                POSITION = "Not played";
+                break;
+            case LADDER:
+                currentPosition += diceNumber;
+                POSITION = "Player is on Ladder";
+                break;
+            case SNAKE:
+                currentPosition -= diceNumber;
+                POSITION = "Player is on Snake";
+                break;
+        }
+        System.out.println("Player's Position :" + POSITION + ",  #Current Position :" + currentPosition);
+        return currentPosition;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
-        playerStartPosition();
-        dieRoll();
+        playerChecksOption();
     }
 }
