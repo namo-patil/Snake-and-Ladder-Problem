74356a7a8773d9833f4d35dcd58c919d08a907db
namo-patil <namratapatil74738@gmail.com>
2022-04-22 22:42:20 +0530

    Added: Program for game played with single player at start position

--- src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladdergame/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.bridgelabz.snakeandladdergame;
 
 public class SnakeAndLadder {
+    public static void playerStartPosition() {
+        int startPosition = 0;
+        System.out.println(startPosition);
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Game");
+        playerStartPosition();
     }
 }
